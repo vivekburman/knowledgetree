c6b5d8e5c9345d4cfef077f1d938d6e753a929a5
Vivek Burman <vivekburman1997@gmail.com>
2022-07-25 20:26:02 +0530

    initial commit

---
+++ src/root.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Root extends Component {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+    }
+    render(): React.ReactNode {
+        return (
+            <div className=""></div>
+        )
+    }
+}
